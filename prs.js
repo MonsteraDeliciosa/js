94ebaef031dd78ce4359e22d8b99f79efd29fb23
Kaja Otrębska <Kaja Otrbska>
2016-07-31 21:34:40 +0200

    modify prs - paper rock scissors

--- prs.js
+++ prs.js
@@ -1,14 +1,17 @@
 function letsPlay(userChoice, computerChoice) {
     var userChoice = prompt("Do you choose rock, paper or scissors?");
     var computerChoice = (Math.random()) * 3;
-    if (computerChoice <= 1) {
-        computerChoice = "rock";
-    } else if (computerChoice <= 2) {
-        computerChoice = "paper";
-    } else {
-        computerChoice = "scissors";
-    }
-    console.log("Computer: " + computerChoice);
+    //translate computerChoice
+        if (computerChoice <= 1) {
+            computerChoice = "rock";
+        } else if (computerChoice <= 2) {
+            computerChoice = "paper";
+        } else {
+            computerChoice = "scissors";
+        }
+        console.log("Computer: " + computerChoice);
+
+    //compare user and computer
     if (userChoice === computerChoice) {
         return "Computer chose " + computerChoice + ". You chose " + userChoice + ". The result is a tie!";
     } else if (userChoice === "paper") {
