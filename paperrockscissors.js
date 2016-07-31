978493bfe643f19dd10b0c1f3cecfd7bac8447ab
Kaja Otrębska <Kaja Otrbska>
2016-07-31 21:08:40 +0200

    add prs - paper rock scissors

---
+++ paperrockscissors.js
@@ -0,0 +1,18 @@
+var userChoice = prompt("choose paper, rock or scissors", "");
+var skynetChoice = (Math.random()) * 3;
+
+if (skynetChoice <= 1) {
+  var computerChoice = "paper";
+}
+
+if (skynetChoice <= 2 && skynetChoice > 1) {
+  var computerChoice = "rock";
+}
+
+if (skynetChoice <= 3 && skynetChoice > 2) {
+  var computerChoice = "scissors";
+}
+
+switch(skynetChoice) {
+  case
+}
---
+++ prs.js
@@ -0,0 +1,35 @@
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = (Math.random()) * 3;
+if (computerChoice <= 1) {
+    computerChoice = "rock";
+} else if (computerChoice <= 2) {
+    computerChoice = "paper";
+} else {
+    computerChoice = "scissors";
+}
+console.log("Computer: " + computerChoice);
+
+var letsPlay = function(choice1, choice2) {
+    if (choice1 === choice2) {
+        return "Computer chose " + computerChoice + ". You chose " + userChoice + ". The result is a tie!";
+    } else if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            return "Computer chose " + computerChoice + ". You chose " + userChoice + ". YOU WIN!";
+        } else {
+            return "Computer chose " + computerChoice + ". You chose " + userChoice + ". YOU LOSE!";
+        }
+    } else if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            return "Computer chose " + computerChoice + ". You chose " + userChoice + ". YOU WIN!";
+        } else {
+            return "Computer chose " + computerChoice + ". You chose " + userChoice + ". YOU LOSE!";
+        }
+    } else if (choice1 === "scissors") {
+        if (choice2 === "paper") {
+            return "Computer chose " + computerChoice + ". You chose " + userChoice + ". YOU WIN!";
+        } else {
+            return "Computer chose " + computerChoice + ". You chose " + userChoice + ". YOU LOSE!";
+        }
+    }
+};
+letsPlay(userChoice, computerChoice);
